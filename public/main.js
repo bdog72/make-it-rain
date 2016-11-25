ee29257074cd23feb3c74553d4dccb1b4b888b68
Brian Bycynski <high5bri@hotmail.com>
2016-11-25 14:48:58 -0500

    Make it rain

--- public/main.js
+++ public/main.js
@@ -1,6 +1,16 @@
-const main = () => {
-  document.getElementById('root').textContent = 'Hello, World!'
+
+const box = document.querySelector('.drop')
+
+setInterval(() => {
+  box.style.top = '0px'
+}, 5000)
+
+const animate = () => {
+  box.style.top = `${parseInt(box.style.top) + 2}px`
 }
 
-document.addEventListener('DOMContentLoaded', main)
+setInterval(animate, 16.66)
 
+setTimeout(() => {
+  box.style.backgroundColor = 'reen'
+}, 5000)
