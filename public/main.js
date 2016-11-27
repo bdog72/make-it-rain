204aad463f8ca117e458ffef6b2230b99a4a5b41
Brian Bycynski <high5bri@hotmail.com>
2016-11-27 05:53:29 -0500

    Make it raine

--- public/main.js
+++ public/main.js
@@ -1,16 +1,38 @@
+const FPS = 50
+const SPEED = 10
 
-const box = document.querySelector('.drop')
-
-setInterval(() => {
-  box.style.top = '0px'
-}, 5000)
-
-const animate = () => {
-  box.style.top = `${parseInt(box.style.top) + 2}px`
+const makeItRain = () => {
+  const main = document.querySelector('main')
+  const drop = document.createElement('img')
+  drop.src = 'raindrop.svg'
+  drop.height = 54
+  drop.style.left = Math.floor(Math.random() * document.body.clientWidth) + 'px'
+  drop.style.top = Math.floor(Math.random() * 50) + 'px'
+  main.appendChild(drop)
 }
 
-setInterval(animate, 16.66)
+const animate = () => {
+  const main = document.querySelector('main')
+  for (let drop of main.children) {
+    const x = Number(drop.style.left.split('px')[0])
+    const y = Number(drop.style.top.split('px')[0])
+    const mod = x % 3
+    if (y < document.body.clientHeight) {
+      drop.style.top = (y + SPEED + mod) + 'px'
+    } else {
+      main.removeChild(drop)
+    }
+  }
+}
 
-setTimeout(() => {
-  box.style.backgroundColor = 'reen'
-}, 5000)
+const main = () => {
+  const button = document.querySelector('button')
+  button.addEventListener('click', (event) => {
+    event.preventDefault()
+    makeItRain()
+  })
+
+  window.setInterval(animate, 1000 / FPS)
+}
+
+document.addEventListener('DOMContentLoaded', main)
